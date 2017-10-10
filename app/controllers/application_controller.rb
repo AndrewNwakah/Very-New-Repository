91f39615e0cba65a4c4db566542d48839006dbd1
Andrew Nwakah <andrewnwakah@gmail.com>
2017-10-10 22:49:36 +0000

    Root to users index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
+  http_basic_authenticate_with name: "dhh", password: "secret"
   def hello
+    
     render html: "hello, world!"
   end
 end
