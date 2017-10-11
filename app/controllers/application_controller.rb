f6556739ffc854f62ffdf59ebe26d6be2d970e56
AndrewNwakah <Andrewnwakah@gmail.com>
2017-10-11 03:11:38 +0000

    root to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
-  http_basic_authenticate_with name: "dhh", password: "secret"
+  http_basic_authenticate_with name: "username", password: "password"
   def hello
     
     render html: "hello, world!"
